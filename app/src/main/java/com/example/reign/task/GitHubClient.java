61bee5fca34747520f563359a1e21b636a1be590
Mohamed Saeed <medosaeed1219@gmail.com>
2017-09-29 00:13:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/reign/task/GitHubClient.java
@@ -0,0 +1,17 @@
+package com.example.reign.task;
+
+import java.util.List;
+//import com.example.reign.model.GitHubRepo;
+
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+/**
+ * Created by Reign on 28-Sep-17.
+ */
+
+public interface GitHubClient {
+   // @GET ("/users/{users}/repos")
+
+    // Call<List<GitHubRepo>> reposForUser( @Path("users") String user);
+}
